043a7d61c5a0364fbea14b091d700e470b4f2a23
Jakub Vala <jamm3e3333@gmail.com>
2023-01-06 00:43:15 +0100

    🐛 Fix passing tests by removing psql logs

--- number_guessing_game/number_guess.sh
+++ number_guessing_game/number_guess.sh
@@ -8,7 +8,7 @@ EXISTING_USER=$($PSQL "select username from users where username='$USERNAME';")
 if [[ -z $EXISTING_USER ]]
 then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
-  $PSQL "insert into users (username) values ('$USERNAME')"  
+  QUERY_RESULT=$( $PSQL "insert into users (username) values ('$USERNAME')" )
 else
   USER_ID=$($PSQL "select user_id from users where username='$USERNAME';")
   GAMES_COUNT=$($PSQL "select count(*) from won_user_games where user_id='$USER_ID';")
@@ -18,13 +18,13 @@ else
 fi
 
 GUESS_COUNT=0
-RANDOM_NUMBER=$(( RANDOM % 10 + 1 ))
-NUMBER_GUESSED=-1
+RANDOM_NUMBER=$(( RANDOM % 1000 + 1 ))
 
-until [ $RANDOM_NUMBER == $NUMBER_GUESSED ]
+echo "Guess the secret number between 1 and 1000:"
+read NUMBER_GUESSED
+
+while [ $RANDOM_NUMBER != $NUMBER_GUESSED ]
 do
-  echo "Guess the secret number between 1 and 1000:"
-  read NUMBER_GUESSED
   if [[ ! $NUMBER_GUESSED =~ ^[0-9]+$ ]]
   then
     echo "That is not an integer, guess again:"
@@ -38,9 +38,11 @@ do
       echo "It's higher than that, guess again:"
     fi
   fi
+  read NUMBER_GUESSED
 done
+(( GUESS_COUNT++ ))
 
 USER_ID=$($PSQL "select user_id from users where username='$USERNAME';")
-$PSQL "insert into won_user_games (user_id, guess_count) values ('$USER_ID', $GUESS_COUNT);"
+QUERY_RESULT=$($PSQL "insert into won_user_games (user_id, guess_count) values ('$USER_ID', $GUESS_COUNT);")
 # finish the game
 echo "You guessed it in $GUESS_COUNT tries. The secret number was $RANDOM_NUMBER. Nice job!"
